f8d6d7e32ebcb6e4d048d36b7373a4b6efc7a130
Evgenii Ryshkov <eryshkov@gmail.com>
2019-05-29 12:04:30 +0300

    MailerTests.php added

---
+++ tests/MailerTests.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class MailerTests extends TestCase
+{
+    public function testValidateEmail()
+    {
+        $this->assertTrue(true);
+    }
+}
