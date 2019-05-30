b6a63d64830f0f5e785e374a8a33dabc3192f207
Evgenii Ryshkov <eryshkov@gmail.com>
2019-05-30 15:46:44 +0300

    Mailer.php fixed

--- src/Services/Mailer.php
+++ src/Services/Mailer.php
@@ -3,33 +3,9 @@
 namespace App\Services;
 
 use Swift_Mailer;
-use Swift_Message;
-use Swift_SmtpTransport;
 
-class Mailer
+class Mailer extends Swift_Mailer
 {
-    public static $userName = 'dwebbo@bk.ru';
-    public static $password = 'pekZes-pykgon-cuqsy3';
-    public static $destination = 'eryshkov@gmail.com';
-    public static $smtpHost = 'smtp.mail.ru';
-    
-    public function mail(string $subjText, string $messageText): int
-    {
-        $transport = new Swift_SmtpTransport(self::$smtpHost, 465);
-        $transport->setEncryption('SSL');
-        $transport->setUsername(self::$userName);
-        $transport->setPassword(self::$password);
-        
-        $mailer = new Swift_Mailer($transport);
-        
-        $message = new Swift_Message($subjText);
-        $message->setFrom([self::$userName => 'robot']);
-        $message->setTo([self::$destination => 'admin']);
-        $message->setBody($messageText);
-        
-        return $mailer->send($message);
-    }
-    
     /**
      * @param string $email
      * @return bool|null
